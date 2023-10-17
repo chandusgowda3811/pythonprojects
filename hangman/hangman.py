04eb0a7f9c7064558c08557bd0580777588c94c2
chandusgowda3811 <139698160+chandusgowda3811@users.noreply.github.com>
2023-10-17 22:37:07 +0530

    Update hangman.py

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,8 +1,6 @@
 import random
 from words import words
-from hangman_visual import lives_visual_dict
 import string
-
 
 def get_valid_word(words):
     word = random.choice(words)  
@@ -20,13 +18,12 @@ def hangman():
 
     lives = 7
 
-    # getting user input
     while len(word_letters) > 0 and lives > 0:
         print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
       
         word_list = [letter if letter in used_letters else '-' for letter in word]
-        print(lives_visual_dict[lives])
+    
         print('Current word: ', ' '.join(word_list))
 
         user_letter = input('Guess a letter: ').upper()
@@ -48,7 +45,6 @@ def hangman():
 
    
     if lives == 0:
-        print(lives_visual_dict[lives])
         print('You died, sorry. The word was', word)
     else:
         print('YAY! You guessed the word', word, '!!')
